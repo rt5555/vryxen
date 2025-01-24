d27656b40a66f0b31b6789c525f9f19d77b2c049
rt5555 <rt5555@users.noreply.github.com>
2025-01-24 20:51:13 -0300

    Improve performance

--- internal/fakerr/fakeerr.go
+++ internal/fakerr/fakeerr.go
@@ -20,3 +20,4 @@ func Show() {
 	text, _ = syscall.UTF16PtrFromString("The code execution cannot proceed because VCRUNTIME140_1.dll was not found. Reinstalling the program may fix this problem.")
 	syscall.NewLazyDLL("user32.dll").NewProc("MessageBoxW").Call(0, uintptr(unsafe.Pointer(text)), uintptr(unsafe.Pointer(title)), 0)
 }
+
